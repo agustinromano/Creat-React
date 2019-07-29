654bc4df870c762a9964bd8904ca2bee139b85f4
agustinromano <48917174+agustinromano@users.noreply.github.com>
2019-07-29 09:25:33 -0400

    Create Card.js

---
+++ Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Card(prop) {
+  return (
+      <div className='Card'>
+          <h3>{prop.title}</h3>
+          <p>{prop.content}</p>
+      </div>
+  );
+}
+
+export default Card;
